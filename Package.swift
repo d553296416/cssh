e9fccc1eb9a56c588c3cfe802bf1916810a0f935
ssh term <admin@ssh2.app>
2025-01-19 21:37:57 +0800

    package

--- Package.swift
+++ Package.swift
@@ -21,12 +21,49 @@ let package = Package(
             name: "OpenSSL",
             targets: ["OpenSSL"]
         ),
+        .library(
+            name: "MaxMindDB",
+            targets: ["MaxMindDB"]
+        ),
     ],
     dependencies: [],
     targets: [
         .target(
             name: "SSHKey",
             dependencies: ["OpenSSL"]
+        ),
+        .target(
+            name: "MaxMindDB",
+            cSettings: [
+                .unsafeFlags(["-w"]),
+                .define("HAVE_ARPA_INET_H"),
+                .define("HAVE_ASSERT_H"),
+                .define("HAVE_CLOCK_GETTIME"),
+                .define("HAVE_DLFCN_H"),
+                .define("HAVE_FCNTL_H"),
+                .define("HAVE_GETPAGESIZE"),
+                .define("HAVE_INTTYPES_H"),
+                .define("HAVE_LIBGEN_H"),
+                .define("HAVE_MATH_H"),
+                .define("HAVE_MMAP"),
+                .define("HAVE_NETDB_H"),
+                .define("HAVE_NETINET_IN_H"),
+                .define("HAVE_OPEN_MEMSTREAM"),
+                .define("HAVE_STDARG_H"),
+                .define("HAVE_STDBOOL_H"),
+                .define("HAVE_STDINT_H"),
+                .define("HAVE_STDIO_H"),
+                .define("HAVE_STDLIB_H"),
+                .define("HAVE_STRINGS_H"),
+                .define("HAVE_STRING_H"),
+                .define("HAVE_SYS_MMAN_H"),
+                .define("HAVE_SYS_PARAM_H"),
+                .define("HAVE_SYS_SOCKET_H"),
+                .define("HAVE_SYS_STAT_H"),
+                .define("HAVE_SYS_TIME_H"),
+                .define("HAVE_SYS_TYPES_H"),
+                .define("HAVE_UNISTD_H"),
+            ]
         ),
         .target(
             name: "CSSH",
@@ -42,7 +79,7 @@ let package = Package(
                 .define("WORDS_BIGENDIAN"),
                 .define("HAVE_POLL"),
                 // .define("HAVE_SELECT"),
-                //.define("HAVE_SYS_SELECT_H"),
+                // .define("HAVE_SYS_SELECT_H"),
                 .define("HAVE_ALLOCA"),
                 .define("HAVE_ALLOCA_H"),
                 .define("HAVE_ARPA_INET_H"),
